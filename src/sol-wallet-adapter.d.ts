36c8233c31ea42a36059c61f0b2f7d2f584f5b41
vuducchinh29 <vuducchinh29@gmail.com>
2021-01-14 16:00:19 +0700

    install and declare: @ezdefi/ezdefi-solala-wallet-adapter

--- src/sol-wallet-adapter.d.ts
+++ src/sol-wallet-adapter.d.ts
@@ -2,3 +2,7 @@ declare module "@project-serum/sol-wallet-adapter" {
   const magic: any;
   export = magic;
 }
+declare module "@ezdefi/ezdefi-solala-wallet-adapter" {
+  const magic: any;
+  export = magic;
+}
